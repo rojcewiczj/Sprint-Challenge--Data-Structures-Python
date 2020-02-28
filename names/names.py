40b843b724cd1167665d7dfc41b7dffd4f9579ef
John Rojcewicz <rojcewiczj@gmail.com>
2020-02-28 10:57:08 -0600

    completed

--- names/names.py
+++ names/names.py
@@ -2,11 +2,11 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('C:/Users/rojce/Documents/GitHub/Sprint-Challenge--Data-Structures-Python/names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('C:/Users/rojce/Documents/GitHub/Sprint-Challenge--Data-Structures-Python/names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
@@ -14,9 +14,9 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,25 +8,81 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+         if self.capacity > 0:
+            self.storage.add_to_tail(item) 
+            self.capacity -= 1 
+            self.count = 0
+
+         else:
+            if self.capacity == 0 and self.count == 0:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.count += 1
+            elif self.count == 1:
+                self.storage.head.insert_after(item)
+                self.storage.head.next.next.delete()
+                self.count +=1
+            elif self.count == 2:
+                self.storage.head.next.insert_after(item)
+                self.storage.head.next.next.next.delete()
+                self.count +=1
+            elif self.count == 3:
+                self.storage.head.next.next.insert_after(item)
+                self.storage.head.next.next.next.next.delete()
+                self.count +=1
+            elif self.count == 4:
+                self.storage.head.next.next.next.insert_after(item)
+                self.storage.head.next.next.next.next.next.delete()
+                self.count = 0
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        
+        node = self.storage.head
+        if type(node.value) is int:
+             list_buffer_contents.append(int(node.value))
+
+        else:
+            list_buffer_contents.append(str(node.value))
+
+        while node.next != None:
+          node = node.next
+          if type(node.value) is int:
+               list_buffer_contents.append(int(node.value))
+          else:  
+               list_buffer_contents.append(str(node.value))
+
 
         return list_buffer_contents
+         
+Ring = RingBuffer(5)
+for i in range(50):
+    Ring.append(i)
+print(Ring.get())      
+
+print(Ring.get())     
+
+print(Ring.get())    
+# Ring.append('h')
+# Ring.append('i')
+# Ring.append('l')
+# Ring.append('a')
+# Ring.append('b')
+print("length: ", Ring.storage.length)
+
 
 # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
